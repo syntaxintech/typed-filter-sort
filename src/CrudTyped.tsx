ff68167ce151f2b8f3f8417b28736d64497378aa
Syntax <edubiosamuelvictor@gmail.com>
2026-06-17 13:49:31 +0100

    CRUD-Typed service with typescript

--- src/CrudTyped.tsx
+++ src/CrudTyped.tsx
@@ -9,7 +9,7 @@ interface IRoutes {
 
 type WayPoints = [latitude: number, longitude: number];
 
-type CreateRoute = Omit<IRoutes, "id" | "createdAt" | "updateAt">;
+type CreateRoute = Omit<IRoutes, "id" | "createdAt" | "updatedAt">;
 type UpdateRoute = Partial<CreateRoute>;
 
 type SuccessResponse<T> = {
@@ -29,25 +29,77 @@ class RouteService {
   private routes: IRoutes[] = [];
 
   // the actions
-  createRoute(input: CreateRoute): ApiResponse<IRoutes> {
-    if (!input.name || !input.waypoints) {
+  public createRoute(input: CreateRoute): ApiResponse<IRoutes> {
+    if (!input || !input.waypoints) {
       return {
         success: false,
-        error: "Missing route name or waypoints",
+        error: "Invalid Input",
       };
     }
 
     const newRoute: IRoutes = {
-      ...input,
       id: Math.random().toString(36).substring(2, 9),
+      ...input,
       createdAt: new Date(),
       updatedAt: new Date(),
     };
     this.routes.push(newRoute);
-
     return {
       success: true,
       data: newRoute,
     };
   }
+
+  public getRouteById(id: string): ApiResponse<IRoutes> {
+    const foundRoute = this.routes.find((route) => route.id === id);
+    if (foundRoute) {
+      return {
+        success: true,
+        data: foundRoute,
+      };
+    } else {
+      return {
+        success: false,
+        error: "Route not found",
+      };
+    }
+  }
+
+  public updatedRoute(id: string, input: UpdateRoute): ApiResponse<IRoutes> {
+    const foundRoute = this.routes.find((route) => route.id === id);
+
+    if (!foundRoute) {
+      return {
+        success: false,
+        error: "Route Not found",
+      };
+    }
+    Object.assign(foundRoute, input, { updatedAt: new Date() });
+
+    return {
+      success: true,
+      data: foundRoute,
+    };
+  }
+
+  public deleteRoute(id: string): ApiResponse<boolean> {
+    const originalLength = this.routes.length;
+
+    this.routes = this.routes.filter((route) => route.id !== id);
+
+    if (this.routes.length === originalLength) {
+      return {
+        success: false,
+        error: "Route not found",
+      };
+    }
+
+    return {
+      success: true,
+      data: true,
+    };
+  }
+}
+function deleteRoute(id: any, string: any) {
+  throw new Error("Function not implemented.");
 }
